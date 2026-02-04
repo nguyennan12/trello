c9620b1405283e6e6112e503705834a437312165
nguyenan12 <nguyenann1204@gmail.com>
2026-02-04 18:12:14 +0700

    update dark mode and save localStorge

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import Button from '@mui/material/Button'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
 
   return (
     <>
+      <ModeToggle />
       <div>test MUI</div>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
---
+++ src/theme.js
@@ -0,0 +1,25 @@
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+
+
+// Create a theme instance.
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        // primary: {
+        //   main: '#ff5252'
+        // }
+      }
+    },
+    dark: {
+      palette: {
+        // primary: {
+        //   main: '#000'
+        // }
+      }
+    }
+  }
+  // ...other properties
+})
+
+export default theme
