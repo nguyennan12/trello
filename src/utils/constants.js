41f9df864ffa6fbad234a0e04960858a779e9dc8
nguyenan12 <nguyenann1204@gmail.com>
2026-02-20 17:22:34 +0700

    fix BUILD_MODE dev and production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,10 @@
-// export const API_ROOT = 'http://localhost:8019'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8019'
+}
 
-export const API_ROOT = 'https://trello-api-doj4.onrender.com'
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-doj4.onrender.com'
+  console.log('🚀 ~ apiRoot:', apiRoot)
+}
+export const API_ROOT = apiRoot
