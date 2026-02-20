5627522ef34b541437eaa06bb727e1fa6d8479f3
nguyenan12 <nguyenann1204@gmail.com>
2026-02-20 22:26:27 +0700

    config Redux for project

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,24 @@ import App from '~/App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from '~/theme'
+
 import { ToastContainer } from 'react-toastify'
-import { ConfirmProvider } from "material-ui-confirm";
+
+import { ConfirmProvider } from "material-ui-confirm"
+
+import { Provider } from 'react-redux'
+import { store } from '~/redux/store'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-  <CssVarsProvider theme={theme}>
-    <ConfirmProvider >
-      <CssBaseline />
-      <App />
-      <ToastContainer autoClose={2000} theme="colored" />
-    </ConfirmProvider>
-  </CssVarsProvider>
+  <Provider store={store}>
+    <CssVarsProvider theme={theme}>
+      <ConfirmProvider >
+        <CssBaseline />
+        <App />
+        <ToastContainer autoClose={2000} theme="colored" />
+      </ConfirmProvider>
+    </CssVarsProvider>
+  </Provider>
 
 )
---
+++ src/redux/activeBoard/activeBoardSlice.js
@@ -0,0 +1,70 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+import { isEmpty } from 'lodash'
+import { API_ROOT } from '~/utils/constants'
+import { generatePlaceholderCard } from '~/utils/formatters'
+import { mapOrder } from '~/utils/sorts'
+
+
+//khởi tạo giá trị State của 1 Slice
+const initialState = {
+  currentActiveBoard: null
+}
+
+// Các lần gọi API (bất đồng bộ) và cập nhật dữ liệu vào Redux, dùng Middleware createAsyncThunk kèm với extraReducers
+export const fetchBoardDetailsAPI = createAsyncThunk(
+  'activeBoard/fetchBoardDetailsAPI',
+  async (boardId) => {
+    const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`)
+    return response.data
+  }
+)
+
+//khởi tạo nột Slice trong Redux Store
+export const activeBoardSlice = createSlice({
+  name: 'activeBoard',
+  initialState,
+  //Reducers: nơi xử lý dữ liệu đồng bộ
+  reducers: {
+    updateCurrentActiveBoard: (state, action) => {
+      //action.payload là chuẩn đặt tên nhận dữ liệu reducers, gáng nó lại 1 biến có nghĩa hơn là board
+      const board = action.payload
+
+      //xử lý dữ liệu
+
+      //update lại dữ liệu
+      state.currentActiveBoard = board
+    }
+  },
+  //ExtraReducers: nơi xử lý dữ liệu bất đồng bộ
+  extraReducers: (builder) => {
+    builder.addCase(fetchBoardDetailsAPI.fulfilled, (state, action) => {
+      //action.payload = response.data
+      let board = action.payload
+
+      //xử lý dữ liệu
+      board.columns = mapOrder(board.columns, board.columnOrderIds, '_id')
+
+      board.columns.forEach(column => {
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceholderCard(column)]
+          column.cardOrderIds = [generatePlaceholderCard(column)._id]
+        } else {
+          column.cards = mapOrder(column.cards, column.cardOrderIds, '_id')
+        }
+      })
+      //update lại dữ liệu
+      state.currentActiveBoard = board
+    })
+  }
+})
+
+// Action: nơi dành cho các components bên dưới gọi bằng dispath() tới để cập nhật dữ liệu thông qua reducer (chạy đồng bộ). action là property có sẵn từ các reducer func
+export const { updateCurrentActiveBoard } = activeBoardSlice.actions
+
+// Selector: nơi dành cho các components bên dưới gọi bằng hook useSelector() để lấy dữ liệu trong Redux Store ra sử dụng
+export const selectCurrentActiveBoard = (state) => {
+  return state.activeBoard.currentActiveBoard
+}
+
+export const activeBoardReducer = activeBoardSlice.reducer
